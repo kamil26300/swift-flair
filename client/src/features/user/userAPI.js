79872f52a186aba8f7cdd2cc1e6cef6ffb223e3f
Kamil Dehliwala <walakamil26@gmail.com>
2024-10-04 22:05:40 +0530

    small issue fixed

--- client/src/features/user/userAPI.js
+++ client/src/features/user/userAPI.js
@@ -1,6 +1,6 @@
 export function fetchLoggedInUserOrders(userId) {
   return new Promise(async (resolve) => {
-    const response = await fetch("/orders?user=" + userId);
+    const response = await fetch("/api/orders?user=" + userId);
     const data = await response.json();
     resolve({ data });
   });

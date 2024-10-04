b6d6774bda4d7bb934fa53626952b097259d53bd
Kamil Dehliwala <walakamil26@gmail.com>
2024-10-04 19:41:27 +0530

    loading skeleton added

--- client/src/features/user/components/UserOrder.js
+++ client/src/features/user/components/UserOrder.js
@@ -5,18 +5,21 @@ import {
   fetchLoggedInUserOrdersAsync,
   selectUserOrders,
   selectUserInfo,
+  selectUserStatus,
 } from "../userSlice";
+import Skeleton from "../../../components/loading/Skeleton";
 
 const UserOrder = () => {
   const dispatch = useDispatch();
   const orders = useSelector(selectUserOrders);
+  const status = useSelector(selectUserStatus);
   const user = useSelector(selectUserInfo);
 
   useEffect(() => {
     dispatch(fetchLoggedInUserOrdersAsync(user.id));
   }, [dispatch, user]);
 
-  const handleReview = (order) => {    
+  const handleReview = (order) => {
     console.log("Handle Review" + order.id);
   };
 
@@ -26,30 +29,34 @@ const UserOrder = () => {
 
   return (
     <div className="lg:w-5/6 w-full">
-      <OrdersAccordion
-        orders={orders.orders}
-        buttons={(order) => (
-          <div className="flex sm:flex-col flex-row justify-around gap-4">
-            <button
-              onClick={(e) => handleReview(order)}
-              className="text-[#E74C3C] w-full border border-[#E74C3C] py-2 px-4"
-            >
-              Review
-            </button>
-            <button
-              onClick={(e) => handleReturn(order)}
-              className="text-[#3498DB] w-full border border-[#3498DB] py-2 px-4"
-            >
-              Return
-            </button>
-          </div>
-        )}
-      />
+      {status === "loading" ? (
+        <Skeleton count={1} />
+      ) : (
+        <OrdersAccordion
+          orders={orders}
+          buttons={(order) => (
+            <div className="flex sm:flex-col flex-row justify-around gap-4">
+              <button
+                onClick={(e) => handleReview(order)}
+                className="text-[#E74C3C] w-full border border-[#E74C3C] py-2 px-4"
+              >
+                Review
+              </button>
+              <button
+                onClick={(e) => handleReturn(order)}
+                className="text-[#3498DB] w-full border border-[#3498DB] py-2 px-4"
+              >
+                Return
+              </button>
+            </div>
+          )}
+        />
+      )}
       {/* <Pagination
         totalItems={totalOrders}
         selectedPage={page}
         handlePage={setPage}
-      /> */}
+        /> */}
     </div>
   );
 };

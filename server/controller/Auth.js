c3def2441198bd854f20399c6e0634544e968791
Kamil Dehliwala <walakamil26@gmail.com>
2024-10-04 19:41:55 +0530

    password bcrypt

--- server/controller/Auth.js
+++ server/controller/Auth.js
@@ -1,8 +1,16 @@
 const { User } = require("../model/User");
+const bcrypt = require('bcrypt');
 
 exports.createUser = async (req, res) => {
-  const user = new User(req.body);
   try {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+
+    const user = new User({
+      ...req.body,
+      password: hashedPassword,
+    });
+
     const doc = await user.save();
     res.status(201).json(doc);
   } catch (error) {
@@ -15,19 +23,19 @@ exports.loginUser = async (req, res) => {
     const user = await User.findOne({ email: req.body.email }).exec();
     if (!user) {
       res.status(400).json({ message: "Invalid credentials" });
-    } else if (user.password === req.body.password) {
-      res
-        .status(201)
-        .json({
+    } else {
+      const validPassword = await bcrypt.compare(req.body.password, user.password);
+      if (validPassword) {
+        res.status(201).json({
           id: user.id,
           email: user.email,
           addresses: user.addresses,
-          orders: user.orders,
           name: user.name,
           role: user.role,
         });
-    } else {
-      res.status(401).json({ message: "Invalid credentials" });
+      } else {
+        res.status(401).json({ message: "Invalid credentials" });
+      }
     }
   } catch (error) {
     res.status(400).json(error);
